1c6ae96aa4c385d1f647ee160eb90ea99499dfc8
iara baya <iarabayav@gmail.com>
2021-10-13 22:02:50 -0300

    basic form template, uncontrolled forms

---
+++ forms/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <form>
+      <div>
+        <span>
+          lala
+        </span>
+        <input name='field'/>
+      </div>
+      <input name='field-2'/>
+      <input type='submit' value='Send' />
+    </form>
+  );
+}
+
+export default App;
