055ec48600d7cace9e661e05278c7d4a72b41ac1
iara baya <iarabayav@gmail.com>
2022-04-21 19:00:37 -0300

    form component with basic onchange methods

--- react - linkedin/react-avanzado-1/src/components/form/Componente.js
+++ react - linkedin/react-avanzado-1/src/components/form/Componente.js
@@ -1,7 +1,11 @@
 class Componente extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      item1: '',
+      item2: '',
+      item3: '',
+    };
   }
 
 
@@ -9,19 +13,40 @@ class Componente extends Component {
     e.preventDefault();
   }
 
+  onNombreInputChange = (e) => {
+    // console.log(e.target.value);
+    const value = e.target.value;
+    this.setState({item1: value})
+  }
+  onApellidoInputChange = (e) => {
+    // console.log(e.target.value)
+    const value = e.target.value;
+    this.setState({item2: value})
+  }
+  onHobbyInputChange = (e) => {
+    console.log(e.target.value)
+    const value = e.target.value;
+    this.setState({item3: value})
+  }
+
   render() {
     return (
       <div>
         <h1>Ingreso Club</h1>
         <form onSubmit={this.onSubmitHandler}>
-          <input type="text" name="item1" placeholder="Nombre" />
+          <input type="text" name="item1" placeholder="Nombre" onChange={this.onNombreInputChange}/>
           
-          <input type="text" name="item2" placeholder="Apellido" />
+          <input type="text" name="item2" placeholder="Apellido" onChange={this.onApellidoInputChange}/>
           
-          <input type="text" name="item3" placeholder="Hobby" />
+          <input type="text" name="item3" placeholder="Hobby" onChange={this.onHobbyInputChange} />
           
           <button type="submit">Guardar</button>
         </form>
+
+        <div>
+          <h1>Resultados</h1>
+          <span>valor 1: {this.state.item1}</span>
+        </div>
       </div>
     );
   }
