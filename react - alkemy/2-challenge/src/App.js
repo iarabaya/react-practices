e497b992c6fda36bb77866bd348f7b26c2e5a05f
iara baya <iarabayav@gmail.com>
2022-04-22 15:08:58 -0300

    challenge with material-ui

---
+++ react - alkemy/2-challenge/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/pages/Login/Login';
+import HomePage from './components/pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hero</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/home" element={<HomePage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
