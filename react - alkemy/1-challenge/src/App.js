32b15f583d1c25bd0a001874036419d8baf4284a
iara baya <iarabayav@gmail.com>
2022-04-28 19:39:28 -0300

    header and footer integrated, bootstrap css added

--- react - alkemy/1-challenge/src/App.js
+++ react - alkemy/1-challenge/src/App.js
@@ -1,16 +1,28 @@
+//Libraries
 import { Route , Routes } from 'react-router-dom';
 
+//Components
 import Login from "./components/Login/Login";
 import List from "./components/List/List";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+//Styles
+// import './css/App.css';
+import './css/bootstrap.min.css'
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route exact path='/' element={<Login/>}/>
-      <Route path='/list' element={<List/>}/>
-    </Routes>
-    </>
+    <div className='container'>
+      <Header/>
+
+      <Routes>
+        <Route exact path='/' element={<Login/>}/>
+        <Route path='/list' element={<List/>}/>
+      </Routes>
+
+      <Footer/>
+    </div>
   );
 }
 
